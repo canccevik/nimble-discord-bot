5d1e8a7c936b5b3ebbab34267c527760196acd43
Can Çevik <mazlumcancevik@gmail.com>
2023-06-29 02:27:10 +0300

    feat: add stop command

--- src/modules/bot/commands/index.ts
+++ src/modules/bot/commands/index.ts
@@ -1,3 +1,4 @@
 import { QuestionsCommand } from './questions'
+import { StopCommand } from './stop'
 
-export const commands = [QuestionsCommand]
+export const commands = [QuestionsCommand, StopCommand]
---
+++ src/modules/bot/commands/stop/stop.command.ts
@@ -0,0 +1,22 @@
+import { Command, Handler, InteractionEvent } from '@discord-nestjs/core'
+import { Interaction } from 'discord.js'
+import { getVoiceConnection } from '@discordjs/voice'
+
+@Command({
+  name: 'durdur',
+  description: 'Botun oynattığı sesi durdurur.'
+})
+export class StopCommand {
+  @Handler()
+  public run(@InteractionEvent() interaction: Interaction): string {
+    const voiceConnection = getVoiceConnection(interaction.guildId)
+
+    if (!voiceConnection) {
+      return `Zaten bir ses kanalında değilim! <@${interaction.user.id}>`
+    }
+
+    voiceConnection.destroy()
+
+    return 'Ses kanalından ayrıldım!'
+  }
+}
